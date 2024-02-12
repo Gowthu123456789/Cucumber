2577da420899ef1107126b13bfa75539ff900ca8
Gowthu123456789 <158144619+Gowthu123456789@users.noreply.github.com>
2024-02-12 07:31:11 +0000

    comment added

--- src/test/java/runner/Addparam.java
+++ src/test/java/runner/Addparam.java
@@ -2,7 +2,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//if we want to run all features give path till feature
 @CucumberOptions(features = "src\\test\\resources\\features\\addparam.feature",glue = "stepDefinations")
 public class Addparam extends AbstractTestNGCucumberTests
 {
